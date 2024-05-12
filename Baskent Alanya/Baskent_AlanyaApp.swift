9e137ec4e358dca4040c26c12a43d439218ed9ba
Eren <erenkaraca2005@gmail.com>
2024-05-12 08:33:45 +0300

    Initial Commit

---
+++ Baskent Alanya/Baskent_AlanyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baskent_AlanyaApp.swift
+//  Baskent Alanya
+//
+//  Created by Eren Karaca on 9.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Baskent_AlanyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baskent AlanyaUITests/Baskent_AlanyaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Baskent_AlanyaUITestsLaunchTests.swift
+//  Baskent AlanyaUITests
+//
+//  Created by Eren Karaca on 9.05.2024.
+//
+
+import XCTest
+
+final class Baskent_AlanyaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
